3c2cac2019629a18c4a053c7820f8441d2e763fe
Omar Aboulgadayel <omar.aboulgadayel@gmail.com>
2024-10-06 14:07:35 +0200

    Fix test_two_qubits()

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from victor_thesis_landscapes import *
 from victor_thesis_metrics import *
+from qnns.cuda_qnn import *
+from qnns.qnn import *
 
 
 def calculate_metrics(landscape):
@@ -16,37 +18,37 @@ def calculate_metrics(landscape):
     print("Inverse Standard Gradient Deviation: " + str(inverse_standard_gradient_deviation))
 
 
-# test landscape generation and metric calculation for QNNs with one qubit
 def test_one_qubit():
-    qnn = get_qnn("CudaU2", list(range(1)), 1, device="cpu")
-    print("Number of Parameter: " + str(len(qnn.params)))
+    '''
+    Test landscape generation and metric calculation for QNNs with one qubit
+    '''
+    qnn = get_qnn(qnn_name="CudaU2", x_wires=[0], num_layers=1, device="cpu")
+    print(f"Number of parameters: {str(len(qnn.params))}")
 
-    unitary = torch.tensor(np.array([[1, 1], [1, -1]]) / np.sqrt(2), dtype=torch.complex128, device="cpu")
-    inputs = generate_random_datapoints(3, 1, unitary)
-    landscape = generate_2d_loss_landscape(10, inputs, unitary, qnn)
-    calculate_metrics(landscape)
+    U = torch.tensor(data=np.array([[1, 1], [1, -1]]) / np.sqrt(2), dtype=torch.complex128, device="cpu")
+
+    inputs = generate_random_datapoints(numb_points=2, s_rank=1, U=U)
+
+    loss_landscape = generate_2d_loss_landscape(grid_size=50, inputs=inputs, U=U, qnn=qnn)
+
+    calculate_metrics(loss_landscape)
 
 
-# test landscape generation and metric calculation for QNNs with two qubits
-# NOT YET WORKING
 def test_two_qubits():
-    qnn = get_qnn("CudaU2", list(range(2)), 3, device="cpu")
-    print("Number of Parameter: " + str(len(qnn.params)))
+    '''
+    Test landscape generation and metric calculation for QNNs with two qubits
+    '''
+    num_qubits = 2
+    qnn = CudaPennylane(num_wires=num_qubits, num_layers=1, device='cpu')
+    print(f"Number of parameters: {str(len(qnn.params))}")
 
-    matrix = np.array([[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
-    unitary = torch.tensor(matrix, dtype=torch.complex128, device="cpu")
-    print(unitary)
+    U = torch.tensor(data=np.array(random_unitary_matrix(num_qubits)), dtype=torch.complex128, device="cpu")
 
-    random_unitary = torch.tensor(np.array(random_unitary_matrix(2)), dtype=torch.complex128, device="cpu")
-    print(random_unitary)
+    inputs = generate_random_datapoints(numb_points=3, s_rank=num_qubits, U=U)
 
-    inputs = generate_random_datapoints(3, 2, random_unitary)
-    print(inputs)
+    loss_landscape = generate_loss_landscape(grid_size=3, dimensions=6, inputs=inputs, U=U, qnn=qnn)
 
-    landscape = generate_2d_loss_landscape(10, inputs, unitary, qnn)
-    print(landscape)
-
-    calculate_metrics(landscape)
+    calculate_metrics(loss_landscape)
 
 
 def test(qnn_type, num_qubits, num_layers, unitary):
@@ -59,13 +61,14 @@ def test(qnn_type, num_qubits, num_layers, unitary):
     calculate_metrics(landscape)
 
 
-
-
 matrix = np.array([[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
 unitary = torch.tensor(matrix, dtype=torch.complex128, device="cpu")
 # test("Pennylane", 2, 3, unitary)
 
 
 if __name__ == "__main__":
+    print("\nOne qubit:")
     test_one_qubit()
-    # test_two_qubits()
+
+    print("\nTwo qubits:")
+    test_two_qubits()
